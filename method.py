3fc336ee23b7ba4616f57578e1379aef8d4e7c15
chanwoo95 <cksdndbd95@gmail.com>
2021-09-20 19:25:45 +0900

    basic function

--- method.py
+++ method.py
@@ -1,3 +1,17 @@
 example = "is it snowing?"
 wordChange = example.replace("snowing", "raining")
 print(wordChange)
+
+#
+
+word = "python algorithm"
+print(word.find("algo"))
+
+#
+
+a1 = "what's your job?"
+a2 = "WHAT'S YOUR JOB?"
+b = a1.upper()
+c = a2.lower()
+print(b)
+print(c)
